6783af87cdc73004a1d07b4719137952d37e4d42
ningsuhen <ningsuhen@gmail.com>
2015-05-10 12:26:52 +0530

    fix bug - should call docker -v instead of npm -v

--- 05update_docker.sh
+++ 05update_docker.sh
@@ -10,7 +10,7 @@
 echo "checking docker..."
 if [ ! -f "/opt/elasticbeanstalk/docker_updated" ]; then
 yum update docker -y && touch /opt/elasticbeanstalk/docker_updated
-echo "YAY! Updated global NPM version to `npm -v`"
+echo "YAY! Updated global NPM version to `docker -v`"
 else
   echo "Skipping NPM -g version update. To update, please uncomment 40install_node.sh:12"
 fi
